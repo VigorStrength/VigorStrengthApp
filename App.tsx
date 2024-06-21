fe75ce23fcbd94dabf3a2cf64aade78e5867dd03
GhostDrew11 <armelhell@gmail.com>
2024-06-21 19:22:39 -0400

    Colors and Fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { useEffect, useState } from "react";
 
-export default function App() {
+const App = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        Satoshi: require("./assets/fonts/Satoshi/Satoshi-Regular.otf"),
+        IntegralCF: require("./assets/fonts/Integral-CF/Fontspring-DEMO-integralcf-regular.otf"),
+      });
+      setFontLoaded(true);
+    };
+
+    loadFonts();
+  }, []);
+
+  if (!fontLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: "Satoshi", fontSize: 24 }}>Hello Satoshi</Text>
+      <Text style={{ fontFamily: "IntegralCF", fontSize: 24 }}>
+        Hello Integral CF
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ GlobalStyles.ts
@@ -0,0 +1,21 @@
+export const Colors = {
+  // Neutral
+  white: "#FFFFFF",
+  neutral100: "#FDFDFD",
+  neutral150: "#F2F2F2",
+  neutral200: "#E9E9E9",
+  neutral250: "#CFD0D0",
+  neutral300: "#B4B5B5",
+  neutral350: "#A3A5A5",
+  neutral400: "#909191",
+  neutral500: "#7E8080",
+  neutral600: "#555859",
+  neutral700: "#444747",
+  neutral800: "#303030",
+  neutral900: "#0F0F0F",
+
+  // Primary
+  orange60: "#FFBF90",
+  orange80: "#FF9F59",
+  orange100: "#FF832A",
+};
