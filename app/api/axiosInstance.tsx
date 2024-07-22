72fa36a89ce80d684c8c7b7de4997eb116bd9110
GhostDrew11 <armelhell@gmail.com>
2024-07-22 19:27:15 -0400

    Update Local Dependencies as well as local network address

--- app/api/axiosInstance.tsx
+++ app/api/axiosInstance.tsx
@@ -1,8 +1,19 @@
 import axios from "axios";
 
-export const baseURL = "http://10.0.0.124:8080/api/v1";
+export const baseURL = "http://10.0.0.126:8080/api/v1";
 
 const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
+  timeout: 5000,
+});
+
+axiosInstance.interceptors.request.use((request) => {
+  console.log("Starting Request", request);
+  return request;
+});
+
+axiosInstance.interceptors.response.use((response) => {
+  console.log("Response:", response);
+  return response;
 });
